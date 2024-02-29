6c94a09ca194968ccd2ad640f7e3eebea4509efb
BlazeStrike228 <161383425+BlazeStrike228@users.noreply.github.com>
2024-02-29 14:57:27 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,1 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nom = $_POST['nom'] ?? '';
-    $discord = $_POST['discord'] ?? '';
 
-    echo "Nom: " . $nom . "<br>";
-    echo "Discord: " . $discord . "<br>";
-} else {
-    echo "Méthode non autorisée.";
-}
-?>
